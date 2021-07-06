43e5160fa35b576ea9f00b11092a78d509dd8509
Kenny <ichigohallow12@gmail.com>
2021-07-06 15:45:50 -0500

    first commit

---
+++ code.js
@@ -0,0 +1,127 @@
+// To run this assignment, right click on index.html in the Visual Studio file explorer to the left
+// and select "Open with Live Server"
+
+/***********************/
+/* Using Variables     */
+/***********************/
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise One"); // Do not modify the lines like this before each exercise, these are to make the console output more readable :)
+// - Print the number 16 to the console.
+//
+// Write your code here 👇
+
+console.log(16)
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Two");
+// - Print the result of the expression six divided by three to the console.
+//
+// Write your code here  👇
+
+ console.log("6 / 3")
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Three");
+// - Add parenthesis to the expression below so that it prints 16 to the console (instead of 12)
+//
+// Write your code here 👇
+
+console.log("1 + 3" * 4);
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Four");
+// - Un-comment the following variable, and then on the next line use console.log() to print it to the console.
+// Write your code here 👇
+
+let myFavoriteNumber = 6;
+console.log ("6")
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Five");
+// - create a variable called "otherNumber", assign the number 5 it, and then print it to the console on the next line.
+//
+// Write your code here 👇
+
+let otherNumber = 5
+console.log("5")
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Six");
+// - Create a variable called "result" and do not assign it any value!  Then log it to the console. (hint: this should print undefined)
+//
+// Write your code here 👇
+
+let result = []
+console.log("result")
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Seven");
+// - Now multiply myFavoriteNumber by otherNumber and assign that to the variable called "result".
+// - Then log it to the console
+// Hint: You can multiply with the * operator: For example, 3 * 5 would be 15
+//
+// Write your code here 👇
+
+console.log(myfavoriteNumber * otherNumber) = result
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Eight");
+// - Create two Variables, called "a" and "b".  Assign the number 5 to "a" and the number 8 to "b".
+// - Then create a third variable, "difference".  Subtract b from a and assign the result to the "difference" variable
+// Hint: subraction uses the - operator
+// Then Print the difference to the console.  (This should print a negative number!)
+//
+// Write your code here 👇
+
+let a = 5
+let b = 8
+let difference = sum
+console.log(sum = "a - b")
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Nine");
+// - For each of the following four things, create a variable with a meaningful name to represent that idea and then assign it a number.
+// - * The number of pets you have
+// - * The number of pairs of sunglasses you own
+// - * The rating from 1-10 you would give to the last meal you ate.
+// - * How many days it has been since you last went to the grocery store.
+// - Remember to use camelCase to name your variables!
+// - Print the value of each of your variables using console.log()
+// Hint: One popular way to name a variable representing a number of something is to name it like "numHats" or "numEggsInFridge"
+//
+// Write your code here 👇
+
+let Animals = 6
+let Shades = 1
+let FoodScore = 7
+let FoodRun = 2
+console.log(sum = "16")
+
+
+
+
+// When you are done, your console output should match the following: (Except your Exercise 9 will be different.)
+/*
+Exercise One
+16
+Exercise Two
+2
+Exercise Three
+16
+Exercise Four
+6
+Exercise Five
+5
+Exercise Six
+undefined
+Exercise Seven
+30
+Exercise Eight
+-3
+Exercise Nine
+2
+3
+9
+12
+*/
